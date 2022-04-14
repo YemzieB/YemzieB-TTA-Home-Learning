0305b2739ca4d44075a750bb006fea7194a4bf2d
YemzieB <98913792+YemzieB@users.noreply.github.com>
2022-04-14 09:01:48 +0100

    Add files via upload

---
+++ Data Academy/laptops.sql
@@ -0,0 +1,39 @@
+
+show databases;
+
+create database laptops;
+
+use laptops;
+
+CREATE TABLE laptops_details(
+skucodes INT NOT NULL,
+brand VARCHAR (20) NOT NULL,
+design VARCHAR (20) NOT NULL,
+OS varchar (20) NOT NULL,
+Processor VARCHAR (20) NOT NULL,
+Ram  INT NOT NULL,              
+Hdd  INT NOT NULL,
+colour VARCHAR (20),
+PRIMARY KEY (skucodes)
+);
+
+explain laptops_details;
+
+alter table laptops_details
+modify design varchar (20);
+
+explain laptops_details;
+
+alter table laptops_details
+add column battery int;
+
+explain laptops_details;
+
+insert into laptops_details ( skucodes, brand, design, OS, Processor, Ram, Hdd, colour, battery)
+Values ('546874', 'HP', 'convertible', 'Windows', 'i7', '8gb', '500gb', 'grey', '9hrs'),
+( '829753', 'Lenovo', 'standard', 'chromebook', 'Ryzen 5', '12gb', '256gb', 'Blue', '8hrs'),
+( '155211', 'Asus', 'touchscreen', 'Windows s', 'i5', '16gb', '1tb', 'silver', '12hrs'),
+('888145','Apple', 'retina display', 'MacOs', 'M1', '32gbgb', '512gb', 'Aluminium', '14hrs'),
+( '778561', 'Dell', 'gaming', 'windows pro', 'Ryzen 7', '48gb', '750gb', 'black', '6hrs');
+
+select * from laptops_details;
